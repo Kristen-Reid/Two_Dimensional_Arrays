a419d59f7d933f8c89905803b0a5714af3efd8c3
Kristen Reid <kreiddetroit@gmail.com>
2025-07-31 20:16:56 -0400

    solved two dimensional arrays

--- two-dimensional.js
+++ two-dimensional.js
@@ -10,15 +10,51 @@
 6. Return sum
 */
 
-const twoDimensionalSum = (arr) => {
-  let sum = 0;
+// const twoDimensionalSum = (arr) => {
+//   let sum = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    let subArr = arr[i];
+//   for (let i = 0; i < arr.length; i++) {
+//     let subArr = arr[i];
 
-    for (let j = 0; j < subArr.length; j++) {
-      sum += subArr[j];
-    }
-  }
-  return sum;
-};
+//     for (let j = 0; j < subArr.length; j++) {
+//       sum += subArr[j];
+//     }
+//   }
+//   return sum;
+// };
+
+/*
+1. Intialize product variable at 1
+2. Iterate through arr in outer loop start index at 0 - less than arr length
+3. Intialize subArr variable with arr[i] elements as value
+4. Iterate through subArr in inner loop starting j index at 0 - less than subArr length
+5. Increment product total by multiplying each subArr[j] element
+6. Return product total
+*/
+
+// const twoDimensionalProduct = arr => {
+//   let product = 1;
+
+//   for (let i = 0; i < arr.length; i++){
+//     let subArr = arr[i];
+
+//     for (let j = 0; j < subArr.length; j++) {
+//       product *= subArr[j];
+//     }
+//   }
+//   return product;
+// };
+
+
+// let arr1 = [
+//     [6, 4],
+//     [5],
+//     [3, 1]
+// ];
+// console.log(twoDimensionalProduct(arr1)); // 360
+
+// let arr2 = [
+//     [11, 4],
+//     [2]
+// ];
+// console.log(twoDimensionalProduct(arr2)); // 88
